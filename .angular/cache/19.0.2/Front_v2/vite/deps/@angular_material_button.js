8f4e3f9dd8b2437d645470177feea1cf4b208db5
Filaret Crainiciuc <81650683+Filaret2000@users.noreply.github.com>
2024-11-27 14:49:58 +0200

    Replace existing files with new ones

---
+++ .angular/cache/19.0.2/Front_v2/vite/deps/@angular_material_button.js
@@ -0,0 +1,33 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-JRK5TIFT.js";
+import "./chunk-5PJ5YUJA.js";
+import "./chunk-SJO2QKKA.js";
+import "./chunk-AJMJUZ6M.js";
+import "./chunk-5TID76VL.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
--- src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'https://localhost:7098' // Adjust this to match your backend URL
-};
