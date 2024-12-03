5a86cf40b27f6d9f8c905bed1c2e90f8e2ad495c
Filaret Crainiciuc <81650683+Filaret2000@users.noreply.github.com>
2024-12-03 18:43:36 +0200

    a

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://proiect-api-demo.com' // Adjust this to match your backend URL
+    apiUrl: 'https://proiect-api-demo.com:7098' // Adjust this to match your backend URL
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://proiect-api-demo.com' // Replace with your production API URL
+    apiUrl: 'https://proiect-api-demo.com:7098' // Replace with your production API URL
 };
